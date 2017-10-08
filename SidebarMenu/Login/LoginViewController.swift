f50873abe5aeb4bec8c92fa95a7e3057b3a9f406
Pavankumar Arepu <iosdeveloper.ipa@gmail.com>
2017-10-08 22:46:34 +0100

    Added Podfile for Google SignIn using Firebase and Added LoginViewController
      Added :
      pod 'Firebase/Core'
      pod 'Firebase/Auth'
      pod 'Firebase/Storage'
      pod 'Firebase/Messaging'
      pod 'Firebase/Auth'
      pod 'GoogleSignIn'

---
+++ SidebarMenu/Login/LoginViewController.swift
@@ -0,0 +1,11 @@
+//
+//  LoginViewController.swift
+//  SidebarMenu
+//
+//  Created by Pavankumar on 08/10/2017.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import GoogleSignIn
---
+++ SidebarMenu/Pods/Target Support Files/Pods-SidebarMenuTests/Pods-SidebarMenuTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SidebarMenuTests : NSObject
+@end
+@implementation PodsDummy_Pods_SidebarMenuTests
+@end
