4af828addeab5a3ad4afe75d9a5c9f1af4fe56b5
Pavankumar Arepu <iosdeveloper.ipa@gmail.com>
2017-10-08 22:53:19 +0100

    Updated BundleID

--- SidebarMenu/SidebarMenu/AppDelegate.swift
+++ SidebarMenu/SidebarMenu/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        // Initialize sign-in
+        var configureError: NSError?
         return true
     }
 
